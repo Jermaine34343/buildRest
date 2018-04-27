370a8001b00f073bdfa558f7b2d559ef1ce509e7
Jermaine34343 <Jermaine34343@gmail.com>
2018-04-27 13:07:56 -0400

    Added Hello

---
+++ src/main/java/com/jermaine/demogreetingapp/DemoGreetingAppApplication.java
@@ -0,0 +1,12 @@
+package com.jermaine.demogreetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGreetingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGreetingAppApplication.class, args);
+	}
+}
